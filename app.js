851a555acf84b96dfa4f7e4e16e46ec910cb7aef
tayyabarif41 <51982376+tayyabarif41@users.noreply.github.com>
2020-08-23 05:13:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyD3BfsEp7Y80NNcAMNlJSWXJqITdjSPzVs",
+  authDomain: "instagram-3fd7d.firebaseapp.com",
+  databaseURL: "https://instagram-3fd7d.firebaseio.com",
+  projectId: "instagram-3fd7d",
+  storageBucket: "instagram-3fd7d.appspot.com",
+  messagingSenderId: "695996872785",
+  appId: "1:695996872785:web:45861fca65fe48c52fe070",
+  measurementId: "G-L2HKD5CS1C"
 };
 
      
